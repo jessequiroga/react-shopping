4f8a0d1f19131be2f85d1416893876bab83bb8d7
David Wang <devtech324@gmail.com>
2020-06-16 14:01:05 +0200

    added top component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Top} from 'components';
 
 export function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <Top/>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                learn learn
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    learn learn learn
-                </a>
+
+                
             </header>
         </div>
     );
---
+++ src/components/Top/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export function Top() {
+    return (
+        <div className="Top">
+            <header className="Top-header">
+                
+                <p>
+                top top top
+                </p>
+ 
+                
+            </header>
+        </div>
+    );
+}
