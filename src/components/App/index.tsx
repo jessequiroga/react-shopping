610d2ae51b0d0e6283d70ac9974b6621d86b6d9f
David Wang <devtech324@gmail.com>
2020-06-17 13:49:30 +0200

    mat ui && top added

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import Typography from '@material-ui/core/Typography';
+import { Container, Box } from '@material-ui/core';
+
 import { Top } from 'components';
 
 export function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <Top />
-                <p>learn learn</p>
-            </header>
-        </div>
+        <Container maxWidth="sm">
+            <Box my={4}>
+                <Top></Top>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    learn learn
+                </Typography>
+            </Box>
+        </Container>
     );
 }
--- src/components/Top/index.tsx
+++ src/components/Top/index.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import {
+    IconButton,
+    Typography,
+    makeStyles,
+    Theme,
+    createStyles,
+    Button,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 2,
+        },
+    }),
+);
 
 export function Top() {
+    const classes = useStyles();
+
     return (
-        <div className="Top">
-            <header className="Top-header">
-                <p>top top top</p>
-            </header>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Toy Shop
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
