3d7575a5c0bb06e3134abc3a9d364cdce48d5704
David Wang <devtech324@gmail.com>
2020-06-18 11:45:59 +0200

    router + ui drawer

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,19 +1,81 @@
 import React from 'react';
 import './App.css';
 import Typography from '@material-ui/core/Typography';
-import { Container, Box } from '@material-ui/core';
+import {
+    Container,
+    makeStyles,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+} from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+import InfoIcon from '@material-ui/icons/Info';
 
-import { Top } from 'components';
+const useStyles = makeStyles((theme) => ({
+    drawerPaper: { width: 'inherit' },
+    link: { textDecoration: 'none', color: theme.palette.text.primary },
+}));
 
 export function App() {
+    const classes = useStyles();
+
     return (
-        <Container maxWidth="sm">
-            <Box my={4}>
-                <Top></Top>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    learn learn
-                </Typography>
-            </Box>
-        </Container>
+        <Router>
+            <div style={{ display: 'flex' }}>
+                <Drawer
+                    style={{ width: '220px' }}
+                    variant="persistent"
+                    anchor="left"
+                    open={true}
+                    classes={{ paper: classes.drawerPaper }}
+                >
+                    <List>
+                        <Link to="/" className={classes.link}>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <HomeIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={'Home'} />
+                            </ListItem>
+                        </Link>
+
+                        <Link to="/about" className={classes.link}>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <InfoIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={'About'} />
+                            </ListItem>
+                        </Link>
+                    </List>
+                </Drawer>
+
+                <Switch>
+                    <Route expect path="/about">
+                        <Container>
+                            <Typography variant="h3" gutterBottom>
+                                About
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                                Lorem ipsum
+                            </Typography>
+                        </Container>
+                    </Route>
+                    <Route expect path="/">
+                        <Container>
+                            <Typography variant="h3" gutterBottom>
+                                Products
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                                Product List
+                            </Typography>
+                        </Container>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
